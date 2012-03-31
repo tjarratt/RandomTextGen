741adf5570ce689e3e8ecacc67081b02ff22e642
Tim Jarratt <tjarratt@nearbuysystems.com>
2012-03-30 21:02:00 -0700

    Couple few enhancements.
    Support calling generate script either with /path/to/seed_text or with the actual
    seed text supplied as ARGV.
    
    Instead of letting the caller supply a seed word (to start the generation), pick a
    random word from the body that we create the markhov chain from.

--- generate.rb
+++ generate.rb
@@ -1,20 +1,27 @@
-#/usr/bin/ruby
 require File.dirname(__FILE__) + '/lib/init'
 
-unless ARGV[0]
-  usage =<<EOF
-  usage: #{File.basename(__FILE__)} <source file>
-         to generate text using <source file> as the seed data
-EOF
-  puts usage
+if ARGV.length == 0
+  script_name = File.basename(__FILE__)
+  puts "usage: #{script_name} <seed word> <source file or raw text>
+         to generate text using as the seed data, starting with <seed word>"
+  puts "example: #{script_name} /path/to/frankenstein.txt"
+  puts "example: #{script_name} hello world i am a simple robot. would you like to play a game?"
   exit(1)
 end
 
-seed_file = ARGV[0]
-seed_text = File.open(File.join(File.dirname(__FILE__), seed_file)) do |f|
-  f.read
+seed_text = nil
+
+if ARGV.length == 1
+  seed_text = File.open(File.join(File.dirname(__FILE__), ARGV[0])) { |f| f.read }
+else
+  seed_text = ARGV.join(' ')
 end
+
+words = seed_text.split(' ')
+seed = words.at(Random.rand(words.length)).gsub(/[^A-Za-z]/, '')
+
+puts "generating from seed #{seed} with a body of size #{words.length}"
 
 text_generator = TextGenerator.new
 text_generator.seed(seed_text)
-puts text_generator.generate(ARGV[1] || "Nearly")
+puts text_generator.generate(seed)
