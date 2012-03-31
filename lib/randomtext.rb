f0d10a49dd937c1d18605fd971747a595a71e12c
Tim Jarratt <tjarratt@nearbuysystems.com>
2012-03-31 15:06:15 -0700

    Add top level module that can be require'd.
    Probably should have let Bundler gem this up for me
    instead of fumbling my way through creating a gem from
    scratch, but I'll consider that a lesson learned.

---
+++ lib/randomtext.rb
@@ -0,0 +1,9 @@
+%w{randomtext/markov_chain randomtext/text_generator randomtext/weighted_directed_graph}.each do |file|
+  require File.expand_path file, File.dirname(__FILE__)
+end
+
+module RandomText
+
+  #this space intentionally left blank
+
+end
